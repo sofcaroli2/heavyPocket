428c22be5653ab8ed9d8ee6b02c6177fc14cf889
Gabriele Ricci <gabriele.ricci7@studio.unibo.it>
2026-01-28 16:35:35 +0100

    update Tags factory for TagCollection

--- src/main/java/it/unibo/heavypocket/mvc/model/core/Tags.java
+++ src/main/java/it/unibo/heavypocket/mvc/model/core/Tags.java
@@ -1,5 +1,7 @@
 package it.unibo.heavypocket.mvc.model.core;
 
+import java.util.Set;
+
 public final class Tags {
 
     private Tags() {
@@ -8,5 +10,9 @@ public final class Tags {
     public static Tag createTag(final String name) {
         // @todo aggiungere validazione dati
         return new TagImpl(name);
+    }
+
+    public static TagCollection createTagCollection(final Set<Tag> tags) {
+        return new TagCollectionImpl(tags);
     }
 }
